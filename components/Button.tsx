ffafb6e5d79f5b183723bb962218e1561bdd3c43
codegatty <rajithgatty61@gmail.com>
2024-09-29 16:44:15 +0530

    --completed getApp section, footer section

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,11 +4,12 @@ type ButtonProps={
     type:'button'|'submit',
     title:string,
     icon?:string,
-    variant:'btn_dark_green'|'btn_green'|'btn_white_text'
+    variant:'btn_dark_green'|'btn_green'|'btn_white_text'|'btn_white'| 'btn_dark_green_outline',
+    full?:boolean
 }
-function Button({type,title,icon,variant}:ButtonProps) {
+function Button({type,title,icon,variant,full}:ButtonProps) {
   return (
-    <button type={type} className={`flexCenter gap-3 rounded-full border ${variant}`}>
+    <button type={type} className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}>
         {icon && <Image src={icon} alt={title} height={24} width={24}/>}
         <label className="bold-16 whitespace-nowrap">{title}</label>
     </button>
